0130e4c09902c1c0e093a6ad5a6cbe8f397cc956
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-11-12 00:10:35 +0530

    too much to leave a message

--- home.js
+++ home.js
@@ -1,27 +1,55 @@
 (function () {
-  // const targetRow = document.querySelector('.categories__row');
+  const categoriesRow = document.querySelector('.categories__row');
 
   const categories = [
-    'Courts',
-    'Policing',
-    'Human Rights',
-    'Child Rights',
-    'Land Rights',
-    'Education',
-    'Health',
-    'Economy',
-    'Migration',
-    'Corporate',
-    'Legal Aid',
-    'Consititution'
+    { name: 'Courts', icon: '/assets/icons/courts.svg' },
+    { name: 'Policing', icon: '/assets/icons/policing.svg' },
+    { name: 'Human Rights', icon: '/assets/icons/humanrights.svg' },
+    { name: 'Child Rights', icon: '/assets/icons/childrights.svg' },
+    { name: 'Land Rights', icon: '/assets/icons/landrights.svg' },
+    { name: 'Education', icon: '/assets/icons/education.svg' },
+    { name: 'Health', icon: '/assets/icons/health.svg' },
+    { name: 'Economy', icon: '/assets/icons/economy.svg' },
+    { name: 'Migration', icon: '/assets/icons/migration.svg' },
+    { name: 'Corporate', icon: '/assets/icons/corporate.svg' },
+    { name: 'Legal Aid', icon: '/assets/icons/legalaid.svg' },
+    { name: 'Consititution', icon: '/assets/icons/constitution.svg' }
   ];
 
-  function categoryTemplate(categoryName) {
-    const categoryElement = document.createElement('div');
-    categoryElement.setAttribute('class', 'col-lg-3 col-md-4 col-sm-4 col-xs-4');
-    categoryElement.innerHTML = categoryName;
-    return categoryElement;
+  function categoryTemplate(category) {
+    return `
+      <div class="col-lg-3 col-md-4 col-sm-4 col-xs-4 category-item">
+        <div class="icon-container">
+          <img src="${category.icon}" alt="${category.name}" />
+        </div>
+        <p>${category.name}</p>
+      </div>
+    `;
   }
 
-  // categories.forEach((cat) => targetRow.appendChild(categoryTemplate(cat)));
+  categories.forEach((category) => categoriesRow.innerHTML += categoryTemplate(category));
+
+  // Code for adding contributors to the homepage
+
+  const contributors = [
+    { name: `AKP Legal`, imagePath: '/assets/company_1.png' },
+    { name: `Legal Check`, imagePath: '/assets/company_2.png' },
+    { name: `Marcelo D'Cruz`, imagePath: '/assets/person_1.png' },
+    { name: `Pavithra K`, imagePath: '/assets/person_2.png' }
+  ];
+
+  function contributorTemplate(contributor) {
+    return `
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+      <div class="contributor-info">
+        <img src="${contributor.imagePath}" alt="${contributor.name}" />
+        <p>${contributor.name}</p>
+      </div>
+    </div>
+    `;
+  }
+
+  const contributorsRow = document.querySelector('.contributors-row');
+
+  contributors.forEach((contributor) => (contributorsRow.innerHTML += contributorTemplate(contributor)));
 })();
